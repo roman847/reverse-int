5a96c75ff82682a034ba9ddbad8e450bd6ff8725
roman847 <romanbogdan147@gmail.com>
2022-01-20 13:56:39 +0300

    feat: add task reverse-init (Thu Jan 20 13:56:39 RTZ 2022)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    function solution(n){
+        let reverseStr =  n.toString().split('').reverse()
+        let res = ''
+        let resNumber = reverseStr.forEach(element => {
+          res+= +element
+          console.log(typeof(res))
+        });
+        return +res
+      }
+      
+        
 }
